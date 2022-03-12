e7c609721bd8dbdab36d3ee2bd91a55ffef136ac
hoshianaaa <hoshina54321@gmail.com>
2022-03-12 22:15:48 +0900

    Euler角追加

--- gui2.py
+++ gui2.py
@@ -10,6 +10,9 @@ import tf
 X = 0
 Y = 0
 Z = 0
+EX = 0
+EY = 0
+EZ = 0
 
 class Window(QWidget):
 
@@ -20,12 +23,12 @@ class Window(QWidget):
     
     def initUI(self):
         
-        self.angle_label = [QLabel() for x in range(4)]
+        self.angle_label = [QLabel() for x in range(6)]
 
         for each_label in self.angle_label:
             each_label.setText(str(0))
 
-        self.angle_slider = [QSlider(Qt.Orientation.Horizontal) for x in range(4)]
+        self.angle_slider = [QSlider(Qt.Orientation.Horizontal) for x in range(6)]
         
         for each_slider in self.angle_slider:
             each_slider.setMaximum(10)
@@ -33,12 +36,12 @@ class Window(QWidget):
             each_slider.setValue(0)
             each_slider.valueChanged.connect(self.value_change)
         
-        servo_frame = [QFrame() for x in range(4)]
-        servo_layout = [QHBoxLayout() for x in range(4)]
+        servo_frame = [QFrame() for x in range(6)]
+        servo_layout = [QHBoxLayout() for x in range(6)]
 
         vbox = QVBoxLayout()
 
-        for i in range(4):
+        for i in range(6):
             servo_layout[i].addWidget(self.angle_label[i])
             servo_layout[i].addWidget(self.angle_slider[i])
 
@@ -51,12 +54,16 @@ class Window(QWidget):
     
     def value_change(self):
         global X,Y,Z
-        for i in range(4):
+        global EX,EY,EZ
+        for i in range(6):
             self.angle_label[i].setText(str(self.angle_slider[i].value()))
 
         X = self.angle_slider[0].value()
         Y = self.angle_slider[1].value()
         Z = self.angle_slider[2].value()
+        EX = self.angle_slider[3].value()
+        EY = self.angle_slider[4].value()
+        EZ = self.angle_slider[5].value()
 
 
 rospy.init_node('tf_publisher_gui')
@@ -70,7 +77,7 @@ while not rospy.is_shutdown():
     QApplication.processEvents()
 
     br.sendTransform(( X, Y, Z),
-                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     tf.transformations.quaternion_from_euler(EX, EY, EZ),
                      rospy.Time.now(),
                      "base_link",
                      "world")
