cebd4009b523d866b0b46cec546a541bb8500395
hoshianaaa <hoshina54321@gmail.com>
2022-03-12 22:00:00 +0900

    add tf sample

---
+++ pub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python  
+import rospy
+
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('turtle_tf_broadcaster')
+
+    br = tf.TransformBroadcaster()
+    r = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+
+        br.sendTransform(( 0, 0, 0),
+                     tf.transformations.quaternion_from_euler(0, 0, 0),
+                     rospy.Time.now(),
+                     "base_link",
+                     "world")
+
+        r.sleep()
